31915d939dc2269161824c7c17ff15e5df29c342
JellyPork <55303387+JellyPork@users.noreply.github.com>
2023-02-21 11:52:17 -0800

    update to CRUD table

--- src/components/DataWorld.vue
+++ src/components/DataWorld.vue
@@ -1,89 +1,148 @@
 <template>
-  
   <v-container>
-    <v-app-bar color="yellow" app>
-      
-      <v-btn icon @click="drawer = !drawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <div>
+      <v-toolbar flat color="white">
+        <v-toolbar-title>My CRUD</v-toolbar-title>
+        <v-divider class="mx-2" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
 
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list>
-        <v-list-item v-for="item in items" :key="item.title" @click="goToPage(item.route)">
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+            <v-card-text>
+              <v-container grid-list-md>
+                <v-layout wrap>
+                  <v-flex xs12 sm6 md4>
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Dessert name"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm6 md4>
+                    <v-text-field
+                      v-model="editedItem.calories"
+                      label="Calories"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm6 md4>
+                    <v-text-field
+                      v-model="editedItem.fat"
+                      label="Fat (g)"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm6 md4>
+                    <v-text-field
+                      v-model="editedItem.carbs"
+                      label="Carbs (g)"
+                    ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 sm6 md4>
+                    <v-text-field
+                      v-model="editedItem.protein"
+                      label="Protein (g)"
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-text>
 
-    <v-table>
-      <thead>
-        <tr>
-          <th class="text-left">
-            Autor
-          </th>
-          <th class="text-left">
-            Titulo
-          </th>
-          <th class="text-left">
-            Posts
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="post in posts" :key="post.title">
-          <td>{{ getUser(post.userId).name }}</td>
-          <td>{{ post.title }}</td>
-          <td>{{ post.body }}</td>
-        </tr>
-        
-      </tbody>
-    </v-table>
-
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+              <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+      <v-data-table :headers="cakes" :items="desserts" class="elevation-1">
+        <template v-slot:items="props">
+          <td>{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.calories }}</td>
+          <td class="text-xs-right">{{ props.item.fat }}</td>
+          <td class="text-xs-right">{{ props.item.carbs }}</td>
+          <td class="text-xs-right">{{ props.item.protein }}</td>
+          <td class="justify-center layout px-0">
+            <v-icon small class="mr-2" @click="editItem(props.item)">
+              edit
+            </v-icon>
+            <v-icon small @click="deleteItem(props.item)"> delete </v-icon>
+          </td>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize">Reset</v-btn>
+        </template>
+      </v-data-table>
+    </div>
   </v-container>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
-      drawer: false,
       group: null,
 
-      items: [
+      dialog: false,
+      cakes: [
         {
-          title: 'Home',
-          route: '/'
+          text: "Dessert (100g serving)",
+          align: "left",
+          sortable: false,
+          value: "name",
         },
-        {
-          title: 'Data',
-          route: '/data-page'
-        }
+        { text: "Calories", value: "calories" },
+        { text: "Fat (g)", value: "fat" },
+        { text: "Carbs (g)", value: "carbs" },
+        { text: "Protein (g)", value: "protein" },
+        { text: "Actions", value: "name", sortable: false },
       ],
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        name: "",
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
+      defaultItem: {
+        name: "",
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
 
       headers: [
-        { text: 'Title', value: 'title' },
-        { text: 'Body', value: 'body' }
+        { text: "Title", value: "title" },
+        { text: "Body", value: "body" },
       ],
       userHeaders: [
-        { text: 'Name', value: 'name' },
-        { text: 'Email', value: 'email' }
+        { text: "Name", value: "name" },
+        { text: "Email", value: "email" },
       ],
 
       posts: [],
       users: [],
       loading: true,
-      search: '',
-
-      
+      search: "",
     };
-    
   },
-
+  created() {
+    this.initialize();
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+  },
   computed: {
     userMap() {
       // Create a map of user IDs to user objects
@@ -91,17 +150,125 @@ export default {
         map[user.id] = user;
         return map;
       }, {});
-    }
+    },
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
   },
   methods: {
-    
-    goToPage(route) {
-      this.$router.push(route)
+    initialize() {
+      this.desserts = [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+        },
+        {
+          name: "Donut",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+        },
+      ];
     },
-    
+
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      const index = this.desserts.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.desserts.splice(index, 1);
+    },
+
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.desserts.push(this.editedItem);
+      }
+      this.close();
+    },
+
+    goToPage(route) {
+      this.$router.push(route);
+    },
+
     async fetchPosts() {
       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
         this.posts = response.data;
         console.log(this.posts);
       } catch (error) {
@@ -111,7 +278,9 @@ export default {
 
     async fetchUsers() {
       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
         this.users = response.data;
         console.log(this.users);
       } catch (error) {
@@ -132,6 +301,6 @@ export default {
   },
   mounted() {
     this.fetchData();
-  }
+  },
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,5 @@
 <template>
   <v-container>
-    
-    <v-app-bar color="yellow" app>
-      
-      <v-btn icon @click="drawer = !drawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list>
-        <v-list-item v-for="item in items" :key="item.title" @click="goToPage(item.route)">
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-row justify="space-around">
       <v-card width="400">
         <v-img
@@ -32,9 +14,12 @@
         <v-card-text>
           <div class="font-weight-bold ms-1 mb-2">Today</div>
 
-          
           <v-card-actions>
-            <v-btn prepend-icon="mdi-vuetify" variant="outlined" @click="reloadImage">
+            <v-btn
+              prepend-icon="mdi-vuetify"
+              variant="outlined"
+              @click="reloadImage"
+            >
               Reload
             </v-btn>
           </v-card-actions>
@@ -48,55 +33,42 @@
 export default {
   data() {
     return {
-      drawer: false,
       group: null,
-      items: [
-        {
-          title: 'Home',
-          route: '/'
-        },
-        {
-          title: 'Data',
-          route: '/data-page'
-        }
-      ],
-      imageUrl: 'https://random.imagecdn.app/500/150',
+
+      imageUrl: "https://random.imagecdn.app/500/150",
       messages: [
         {
-          from: 'Me',
-          time: '8:00 AM',
-          message: 'Good morning!',
-          color: 'green',
+          from: "Me",
+          time: "8:00 AM",
+          message: "Good morning!",
+          color: "green",
         },
         {
-          from: 'Friend',
-          time: '8:15 AM',
-          message: 'Good morning! How are you?',
-          color: 'blue',
+          from: "Friend",
+          time: "8:15 AM",
+          message: "Good morning! How are you?",
+          color: "blue",
         },
         {
-          from: 'Me',
-          time: '8:20 AM',
+          from: "Me",
+          time: "8:20 AM",
           message: "I'm doing well, thanks. How about you?",
-          color: 'green',
+          color: "green",
         },
         {
-          from: 'Friend',
-          time: '8:25 AM',
+          from: "Friend",
+          time: "8:25 AM",
           message: "I'm doing pretty good too. Just a bit tired.",
-          color: 'blue',
+          color: "blue",
         },
       ],
     };
   },
   methods: {
     reloadImage() {
-      this.imageUrl = "https://random.responsiveimages.io/v1/docs" + new Date().getTime();
+      this.imageUrl =
+        "https://random.responsiveimages.io/v1/docs" + new Date().getTime();
     },
-    goToPage(route) {
-      this.$router.push(route)
-    }
-  }
+  },
 };
-  
 </script>
